c629db311db3f11ad591dcc9ecbb741a8af467c9
velandimplementation <velandimplementation@gmail.com>
2026-04-30 16:13:06 +0530

    return

--- velan-dashboard/backend/server.js
+++ velan-dashboard/backend/server.js
@@ -359,7 +359,7 @@ app.use(express.static(path.join(__dirname, '..', 'frontend')));
 app.use((req, res) => {
   res.sendFile(path.join(__dirname, '..', 'frontend', 'index.html'));
 });
-server.listen(PORT, '0.0.0.0', function(){
+server.listen(PORT, function(){
   console.log('\nVelan Dashboard API ready');
   console.log('http://localhost:' + PORT + '/api/health');
 
